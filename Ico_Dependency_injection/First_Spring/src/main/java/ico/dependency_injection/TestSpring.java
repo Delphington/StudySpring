420c0f4f28ed5ca672b527c63604533561dad3f0
Delphington <Delphington@gmail.com>
2024-10-06 14:23:54 +0300

    Ico

--- Ico_Dependency_injection/First_Spring/src/main/java/ico/dependency_injection/TestSpring.java
+++ Ico_Dependency_injection/First_Spring/src/main/java/ico/dependency_injection/TestSpring.java
@@ -4,17 +4,19 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestSpring {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context =  new ClassPathXmlApplicationContext(
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml"
         );
         //TODO: Проблема, что мы в ручную создаем объекты - можно это автоматизировать
-        Music music= context.getBean("ClassicMusic", ClassicMusic.class); //полиморфизм
+        /**
+         Music music= context.getBean("ClassicMusic", ClassicMusic.class); //полиморфизм
+         PlayerMusic pl = new PlayerMusic(music);
+         System.out.println(pl.getMusic()); // классическая ClassicMusic
+         */
 
-        PlayerMusic pl = new PlayerMusic(music);
-        System.out.println(pl.getMusic()); // классическая ClassicMusic
-
-//        TestBean  testBean = context.getBean("testBean", TestBean.class);
-//        System.out.println(testBean.getName());
-//        context.close();
+        //# Решение: (подключаем  зависимости в applicationContext)
+        //Передаем в параметры: id - нашего Бина, и возвращаемый тип
+        PlayerMusic playerMusic = context.getBean("PlayerMusic", PlayerMusic.class);
+        System.out.println(playerMusic.getMusic());  // классическая ClassicMusic
     }
 }
