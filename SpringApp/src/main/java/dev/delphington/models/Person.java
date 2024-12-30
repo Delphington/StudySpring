38a3b99a73e78bbb415ee7f25414caf842134708
Delphington <Delphington@gmail.com>
2024-12-30 09:33:42 +0300

    feat: firstApp

---
+++ SpringApp/src/main/java/dev/delphington/models/Person.java
@@ -0,0 +1,13 @@
+package dev.delphington.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Person {
+    private int id;
+    private String name;
+}
