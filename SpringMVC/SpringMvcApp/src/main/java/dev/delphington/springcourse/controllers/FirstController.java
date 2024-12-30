c5b42559b1196ef02e6550797acb6bbc5036c6d4
Delphington <Delphington@gmail.com>
2024-12-30 15:44:22 +0300

    feat: model

--- SpringMVC/SpringMvcApp/src/main/java/dev/delphington/springcourse/controllers/FirstController.java
+++ SpringMVC/SpringMvcApp/src/main/java/dev/delphington/springcourse/controllers/FirstController.java
@@ -1,22 +1,36 @@
 package dev.delphington.springcourse.controllers;
 
 
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RequestMapping("/first")
 @Controller
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage(){
+    public String helloPage(HttpServletRequest request, Model model) {
+        String name = request.getParameter("name");
+        String age = request.getParameter("age");
+        //System.out.println(name + " " + age);
+
+        model.addAttribute("message", "name: " + name  + " age: " + age);
+
         return "first/hello";
     }
 
 
     @GetMapping("/goodbye")
-    public String goodByePage(){
+    public String goodByePage(@RequestParam(value = "name", required = false) String name,
+                              @RequestParam(value = "age", required = false ) String age) {
+        System.out.println(name + " " + age);
+
         return "first/goodbye";
     }
 
